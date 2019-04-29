16ba29f63d60c4d7cc03b2de37308510de4c6474
snaag <04_miffy@naver.com>
2019-04-29 14:48:50 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends React.Component {
+
+  state = {}
+
+  componentDidMount(){
+    this._getMovies();
+  }
+   
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?quality=3D?order_by=rating')
+    .then(potato => potato.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map(movie => {
+      console.log(movie)
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+        ></Movie>
+    })
+
+    return movies
+  }
+
+  _renderLoading = () => {
+    const url = "https://loading.io/spinners/rolling/index.curve-bars-loading-indicator.gif"
+    return <img src={url}></img>
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.movies ? this._renderMovies() : this._renderLoading() }
+      </div>
+    );
+  }
+} 
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import './Movie.css';
+import PropTypes from 'prop-types';
+
+
+
+
+function Movie({title, poster, genres, synopsis}) {
+    return (
+        <div className="Movie">
+            <div className="Movie__Columns">
+                <MoviePoster poster={poster} />
+            </div>
+
+            <div className="Movie__Columns">
+                <h1>{title}</h1>
+
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+
+                <p className="Movie__Synopsis">
+                    {synopsis}
+                </p>
+            </div>
+
+        </div>
+    )
+}
+
+function MoviePoster({poster}) {
+    return (
+        <img src={poster} alt="Image" /> 
+        // class가 아니라서 this.props를 쓰지 않는다는건 무슨말
+        // 함수형 컴포넌트니까, 파라미터로 받으니까
+    )
+}
+
+function MovieGenre({genre}) {
+    return (
+        <span className="Movie__Genre">{genre}</span>
+    )
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired,
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired,
+}
+
+
+export default Movie;
